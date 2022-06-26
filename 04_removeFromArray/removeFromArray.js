a2f703d841fe7e25ee01cafabb207b04b705a9c4
CodingWithDude <codingwithdude@gmail.com>
2022-06-26 00:29:40 -0700

    Updated removeFromArray, passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    itemsToRemove = Array.from(args)
+    for (let i = 0; i <= itemsToRemove.length; i++) {
+        let myIndex = arr.indexOf(itemsToRemove[i])
+        if (myIndex !== -1) {
+            arr.splice(myIndex,1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
