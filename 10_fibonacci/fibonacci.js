c0dd75a594a91545406a2d0267012d5c784f9135
CodingWithDude <codingwithdude@gmail.com>
2022-07-03 14:12:00 -0700

    Created fib function and passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    input = Number(input)
+    fibArray = [1]
+    let n1 = 0, n2 = 1, nextTerm
+    if (input < 0) {
+        return "OOPS"
+    } else {
+        for (let i = 1; i < input;i++){
+            nextTerm = n1 + n2
+            n1 = n2
+            n2 = nextTerm
+            fibArray.push(nextTerm)
+        }
+    }
+    return fibArray.at(-1)
 
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+ 
