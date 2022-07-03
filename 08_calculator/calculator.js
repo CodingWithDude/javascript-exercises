44e6b1faa31044fbc1f72737084f819643219ab9
CodingWithDude <codingwithdude@gmail.com>
2022-07-03 13:28:42 -0700

    Updated functions and passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1 = 0,num2 = 0) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1 = 0,num2 = 0) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0
+	for (i = 0; i < array.length; i++) {
+    result += array[i]
+  }
+  return result
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let result = 1
+	for (i = 0; i < array.length; i++) {
+    result *= array[i]
+  }
+  return result
+};
+
+const power = function(num1 = 0,num2 = 0) {
+  return num1 ** num2
 
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num = 0) {
+	if (num === 0 || num === 1){
+    return 1
+  } else {
+    return num * factorial(num-1)
+  }
 };
 
 // Do not edit below this line
